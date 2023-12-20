65791e7de9246cd5f143e4316641a5776d60adb2
Jeisson Chacon <Jeissonchaconjp@gmail.com>
2023-12-20 10:06:23 -0500

    Servicio 2

--- src/main/java/com/example/finaltest/controller/PokemonController.java
+++ src/main/java/com/example/finaltest/controller/PokemonController.java
@@ -1,4 +1,22 @@
 package com.example.finaltest.controller;
+import com.example.finaltest.entities.Pokemon;
+import com.example.finaltest.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/pokemons")
 public class PokemonController {
+    @Autowired
+    PokemonRepository pokemonRepository;
+    @GetMapping("/{tipoPokemon}")
+    public Pokemon getPokemonsbyTipo(@PathVariable Integer tipoPokemon) {
+        Optional<Pokemon> pokemon = pokemonRepository.findById(tipoPokemon);
+        if (pokemon.isPresent()) {
+            return pokemon.get();
+        }
+        return null;
+    }
 }
